8e7a7a0680a92e045022e1090af255b767e1ce51
binvant <binvantbajwa@gmail.com>
2022-03-07 19:54:50 -0500

    Commit

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
@@ -13,5 +15,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
 if __name__ == "__main__":
     main()
